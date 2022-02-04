49edf8a45c5ba0b3c699acfb72f73c3d9137d459
Samuel Cifuentes García <uo238524@uniovi.es>
2022-02-04 21:37:47 +0100

    Added dockerfile and docker-compose
    Dockerfile is used to create a jenkins container with maven and the
    compose file to run both jenkins and a tomcat container.

---
+++ web-server/src/main/java/com/srensamblador/dummywebserver/DummyWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.srensamblador.dummywebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyWebServerApplication.class, args);
+	}
+
+}
---
+++ web-server/src/test/java/com/srensamblador/dummywebserver/DummyWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srensamblador.dummywebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
